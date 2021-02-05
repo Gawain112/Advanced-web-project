6780f5364ab5bd92dab58c1f95d617ecc9cef227
chris carr <ccarr895@gmail.com>
2021-02-05 14:47:59 +0000

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import ElementPlus from "element-plus";
+import "./element-variables.scss";
 
 createApp(App)
   .use(router)
+  .use(ElementPlus)
   .mount("#app");
