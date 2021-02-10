562e40a6f218e85bcaf2d71463dc52cc678855e3
chris carr <ccarr895@gmail.com>
2021-02-10 11:53:34 +0000

    initial database work

---
+++ src/firebase/database.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdK51TJ9dL5WuTTqM4TwdCOkCyGLGPbBc",
+  authDomain: "advanced-web-project.firebaseapp.com",
+  projectId: "advanced-web-project",
+  storageBucket: "advanced-web-project.appspot.com",
+  messagingSenderId: "886901891838",
+  appId: "1:886901891838:web:f474c075836f6208befbaf"
+};
+
+firebase.initializeApp(firebaseConfig);
+const firebaseAuthenticationService = firebase.auth();
+const firebaseFireStore = firebase.firestore();
+
+export { firebaseAuthenticationService, firebaseFireStore };
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import firebaseTests from "@/firebase/firebaseTests/FirebaseTests.vue";
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/tests",
+    name: "Tests",
+    component: firebaseTests
   }
 ];
 
