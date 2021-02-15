404f823d77ec2c39b20f7b5ae2d01512c003d221
chris carr <ccarr895@gmail.com>
2021-02-15 10:54:53 +0000

    created login and register tests

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: "ts-jest",
+  globals: {
+    "vue-jest": {
+      tsConfig: "./tsconfig.json"
+    },
+    "ts-jest": {
+      tsConfigFile: "tsconfig.json",
+      enableTsDiagnostics: true
+    }
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1"
+  },
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\js$": "babel-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  moduleFileExtensions: ["vue", "js", "json", "jsx", "ts", "tsx", "node"]
+};
---
+++ src/tests/Login.test.ts
@@ -0,0 +1,36 @@
+import { mount } from "@vue/test-utils";
+import Login from "@/views/account/Login.vue";
+import ElementPlus from "element-plus";
+
+const wrapper = mount(Login, {
+    global: {
+        plugins: [ElementPlus]
+    }
+});
+
+const email = "ccarr895@gmail.com";
+const password = "password"
+
+const usernameInput = wrapper.find("input[type=text]");
+const passwordInput = wrapper.find("input[type=password]");
+
+const submitButton = wrapper.find("button");
+
+
+test("LoginValidateFailTest", async () => {
+    await usernameInput.trigger("blur");
+
+    expect(wrapper.find('.el-form-item__error').exists()).toBe(true);
+
+});
+
+test("LoginValidatePassTest", async () => {
+
+    await usernameInput.setValue(email);
+    await passwordInput.setValue(password);
+
+    await submitButton.trigger("click");
+
+
+    expect(wrapper.find('.el-form-item__error').exists()).toBe(false);
+});
