af2d85f26dbd7e8e78c192a7f81ff091842c1718
chris carr <ccarr895@gmail.com>
2021-02-15 11:58:36 +0000

    configured ide to run plugins

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   globals: {
-    "vue-jest": {
-      tsConfig: "./tsconfig.json"
-    },
     "ts-jest": {
       tsConfig: "tsconfig.json",
       enableTsDiagnostics: true
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -3,34 +3,30 @@ import Login from "@/views/account/Login.vue";
 import ElementPlus from "element-plus";
 
 const wrapper = mount(Login, {
-    global: {
-        plugins: [ElementPlus]
-    }
+  global: {
+    plugins: [ElementPlus]
+  }
 });
 
 const email = "ccarr895@gmail.com";
-const password = "password"
+const password = "password";
 
 const usernameInput = wrapper.find("input[type=text]");
 const passwordInput = wrapper.find("input[type=password]");
 
 const submitButton = wrapper.find("button");
 
-
 test("LoginValidateFailTest", async () => {
-    await usernameInput.trigger("blur");
+  await usernameInput.trigger("blur");
 
-    expect(wrapper.find('.el-form-item__error').exists()).toBe(true);
-
+  expect(wrapper.find(".el-form-item__error").exists()).toBe(true);
 });
 
 test("LoginValidatePassTest", async () => {
+  await usernameInput.setValue(email);
+  await passwordInput.setValue(password);
 
-    await usernameInput.setValue(email);
-    await passwordInput.setValue(password);
+  await submitButton.trigger("click");
 
-    await submitButton.trigger("click");
-
-
-    expect(wrapper.find('.el-form-item__error').exists()).toBe(false);
+  expect(wrapper.find(".el-form-item__error").exists()).toBe(false);
 });
