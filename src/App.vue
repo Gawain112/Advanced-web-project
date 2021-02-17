ffde4108855538587f4c23165df52c535382b5e5
Gawain112 <DaHawk11@googlemail.com>
2021-02-17 20:49:57 +0000

    Header/Footer Split

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,22 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <ul class="nav navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link :to="{ name: 'Home' }" class="nav-link">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'About' }" class="nav-link">About</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'Analysis' }" class="nav-link">Analysis</router-link>
-        </li>
-      </ul>
-
-      <ul class="nav navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link :to="{ name: 'Login' }" class="nav-link">Login</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'Register' }" class="nav-link">Register</router-link>
-        </li>
-      </ul>
-    </nav>
+  <div>
+    <appHeader />
 
     <router-view @loggedIn="logIn" />
 
-    <footer class="page-footer bg-dark mt-3">
-      <div class="footer-copyright text-light text-center py-3">
-        &copy; 2021 MMU - 6G6Z1111_2021_9Z6 - Group 9. All Rights Reserved.
-      </div>
-      <router-link :to="{ name: 'Sitemap' }" class="text-decoration-none text-light">Site Map</router-link>
-    </footer>
+    <appFooter />
+  </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
   name: "App",
+  components: {
+    'appHeader': Header,
+    'appFooter' : Footer
+  },
   methods: {
     logIn(user) {
       console.log(user);
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="app-footer page-footer bg-dark mt-3">
+        <div class="footer-copyright text-light text-center py-3">
+        &copy; 2021 MMU - 6G6Z1111_2021_9Z6 - Group 9. All Rights Reserved.
+        </div>
+        <router-link :to="{ name: 'Sitemap' }" class="text-decoration-none text-light">Site Map</router-link>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      msg: 'Footer'
+    }
+  }
+}
+</script>
