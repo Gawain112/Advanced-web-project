5766d5e857826adfd10be2a221d6022dce3e19d0
chris carr <ccarr895@gmail.com>
2021-02-22 15:39:09 +0000

    completed basic search functionality

--- src/components/search/SearchSynopsis.vue
+++ src/components/search/SearchSynopsis.vue
@@ -1,20 +1,37 @@
 <template>
-  <div v-if="geneInfo.synopsis">
-    <el-select v-model="selectedSynopsis" @change="log()">
-      <el-option
+  <el-card class="mt-2">
+    <template #header>
+      <h1>{{ toSearch }}</h1>
+    </template>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <div v-else-if="geneInfo.synopsis">
+      <div
         v-for="(key, value) in geneInfo.synopsis"
         :key="value"
         :value="value"
         :text="key"
-        >{{ key }}</el-option
       >
-    </el-select>
-  </div>
+        <el-row
+          ><p class="text-secondary">{{ key }}</p></el-row
+        >
+      </div>
+    </div>
+  </el-card>
 </template>
 
 <script>
 import { ref } from "vue";
 export default {
+  props: {
+    toSearch: {
+      name: "Search Phrase",
+      type: String,
+      required: true
+    }
+  },
+  name: "Synopsis Search",
   setup(props) {
     const geneInfo = ref({});
     geneInfo.value.synopsis = {};
@@ -22,28 +39,23 @@ export default {
     const omimUrlBaseUrl = "https://api.omim.org/api";
     let clinicalSynopsisUrl =
       omimUrlBaseUrl +
-      "/clinicalSynopsis/search?format=json&apiKey=Jm05PtgLRD2GxaGYfr4xxg&limit=20&search="+props.toSearch;
+      "/clinicalSynopsis/search?format=json&apiKey=Jm05PtgLRD2GxaGYfr4xxg&limit=20&search=" +
+      props.toSearch;
     window
       .fetch(clinicalSynopsisUrl)
       .then(async res => {
         return res.json();
       })
       .then(async json => {
+        console.log(json);
         for (let key in json.omim.searchResponse.clinicalSynopsisList) {
           let synopsis = json.omim.searchResponse.clinicalSynopsisList[key];
           geneInfo.value.synopsis[synopsis.clinicalSynopsis.mimNumber] =
             synopsis.clinicalSynopsis.preferredTitle;
         }
-        console.log(geneInfo.value);
       });
 
-    const selectedSynopsis = ref("");
-
-    let log = () => {
-      console.log(selectedSynopsis.value);
-    };
-
-    return { geneInfo, selectedSynopsis, log };
+    return { geneInfo };
   }
 };
 </script>
