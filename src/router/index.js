0b66486b1086c48bc6591a7108161120a5c5f29a
chris carr <ccarr895@gmail.com>
2021-02-23 12:38:44 +0000

    initial graph work

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Analysis from "@/views/Analysis.vue";
 import firebaseTests from "@/firebase/firebaseTests/FirebaseTests.vue";
-import Search from "@/views/search/Search.vue";
 import Sitemap from "@/views/Sitemap.vue";
 import Login from "@/views/account/Login.vue";
 import Register from "@/views/account/Register.vue";
 import NotFound from "@/views/NotFound.vue";
+import Graph from "@/views/Graph.vue";
 
 const routes = [
   {
@@ -19,73 +19,69 @@ const routes = [
       metaTags: [
         {
           name: "description",
-          content: "Your number 1 cardiomyopathy data and analysis site."
+          content: "Your number 1 cardiomyopathy data and analysis site.",
         },
         {
           property: "og:description",
-          content: "Your number 1 cardiomyopathy data and analysis site."
-        }
-      ]
-    }
+          content: "Your number 1 cardiomyopathy data and analysis site.",
+        },
+      ],
+    },
   },
   {
     path: "/about",
     name: "About",
     component: About,
-    meta: { title: "About | Cardiomyopathy" }
+    meta: { title: "About | Cardiomyopathy" },
   },
   {
     path: "/analysis",
     name: "Analysis",
     component: Analysis,
-    meta: { title: "Analysis | Cardiomyopathy" }
+    meta: { title: "Analysis | Cardiomyopathy" },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
-    meta: { title: "Login | Cardiomyopathy" }
+    meta: { title: "Login | Cardiomyopathy" },
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
-    meta: { title: "Register | Cardiomyopathy" }
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: Register
+    meta: { title: "Register | Cardiomyopathy" },
   },
   {
     path: "/tests",
     name: "Tests",
     component: firebaseTests,
-    meta: { title: "Tests | Cardiomyopathy" }
+    meta: { title: "Tests | Cardiomyopathy" },
   },
   {
     path: "/sitemap",
     name: "Sitemap",
     component: Sitemap,
-    meta: { title: "Site Map | Cardiomyopathy" }
-  },
-  {
-    path: "/search",
-    name: "Search",
-    component: Search
+    meta: { title: "Site Map | Cardiomyopathy" },
   },
   {
     path: "/:catchAll(.*)",
     redirect: "/",
     name: "Not Found",
     component: NotFound,
-    meta: { title: "Not Found | Cardiomyopathy" }
-  }
+    meta: { title: "Not Found | Cardiomyopathy" },
+  },
+  {
+    path: "/graph",
+    name: "Graph",
+    meta: { title: "Graph | Cardiomyopathy" },
+    component: Graph,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
