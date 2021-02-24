9112acca88581ab2d1952bb1ac182df816bedd40
chris carr <ccarr895@gmail.com>
2021-02-24 20:13:23 +0000

    github pages

--- src/views/Analysis.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="analysis">
-    <h1>This is the Analysis page</h1>
-  </div>
-</template>
--- src/views/Sitemap.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="sitemap">
-    <h1>Sitemap</h1>
-    <img alt="Sitemap" src="../assets/sitemap.png" style="width:100%" />
-  </div>
-</template>
