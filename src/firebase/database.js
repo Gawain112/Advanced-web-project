9d42ab6754152940dedbff92116620ddd291eb8d
Gawain112 <DaHawk11@googlemail.com>
2021-02-24 12:40:53 +0000

    Database

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCdK51TJ9dL5WuTTqM4TwdCOkCyGLGPbBc",
-  authDomain: "advanced-web-project.firebaseapp.com",
-  projectId: "advanced-web-project",
-  storageBucket: "advanced-web-project.appspot.com",
-  messagingSenderId: "886901891838",
-  appId: "1:886901891838:web:f474c075836f6208befbaf"
+  apiKey: "AIzaSyAhQEw9MtCtYvdx4fHHjbULW4EU23_YibA",
+  authDomain: "cardiomyopathy-feed.firebaseapp.com",
+  projectId: "cardiomyopathy-feed",
+  storageBucket: "cardiomyopathy-feed.appspot.com",
+  messagingSenderId: "892710548509",
+  appId: "1:892710548509:web:ec102a131cdb22f09302f4"
 };
 
 firebase.initializeApp(firebaseConfig);
