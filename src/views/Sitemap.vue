721ed82daac1495d5a6dbbd16484ea0e51d65de0
Gawain112 <DaHawk11@googlemail.com>
2021-02-26 09:16:46 +0000

    sitemap update

--- src/views/Sitemap.vue
+++ src/views/Sitemap.vue
@@ -2,6 +2,6 @@
   <el-main
     ><div class="sitemap">
       <h1>Sitemap</h1>
-      <img alt="Sitemap" src="../assets/sitemap.png" style="width:100%" /></div
+      <img alt="Sitemap" src="../assets/sitemap1.png" style="width:100%" /></div
   ></el-main>
 </template>
